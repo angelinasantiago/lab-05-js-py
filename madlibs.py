c709918debe10db7a2dfb30da00e1da8fb5843aa
ANGELINA <ang@MacBook-Air.home>
2021-03-08 17:00:28 -0500

    madlibs python file completed

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Enter a noun...") # this version includes user submitted values
+adjective = input("Enter a adjective...")
+adverb = input("Enter a adverb...")
+verb = input("Enter a verb...")
+
+print("On a tropical " + str(noun) + ",")
+print("Underneath a " + str(adjective) + " lava moon.")
+print("Hangin' " + str(adverb) + " with the hula dancers," )
+print(str(verb) + " questions because they all got the answers." )
